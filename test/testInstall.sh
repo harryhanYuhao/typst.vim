c16b11e097dab99756e7e358b4e46a3312e50c9e
Harry Yuhao Han <s2162783@ed.ac.uk>
2023-11-12 11:00:35 +0000

    fix docker

--- test/testInstall.sh
+++ test/testInstall.sh
@@ -13,3 +13,11 @@ if [[ -d $HOME/.vim/ || -f $HOME/.vimrc ]]; then
 	fi
 fi
 
+echo "Back up will be created, but not backup of backup"
+
+mv $HOME/.vim/ $HOME/.vim.bak/
+mv $HOME/.vimrc $HOME/.vimrc.bak
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/vimrc --create-dirs \
+	https://raw.githubusercontent.com/harryhanYuhao/typst.vim/main/test/minimalVimrc
